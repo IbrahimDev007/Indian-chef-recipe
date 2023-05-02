c9e174064951a9698fe67cd4913251ab7b8b982f
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-05-02 22:58:17 +0600

    done regestration with google authentication and email

--- src/Component/Registration.jsx
+++ src/Component/Registration.jsx
@@ -1,7 +1,119 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
+// import { Input, Button, Card, Form, Alert } from "daisyui";
 
 const Registration = () => {
-	return <div>this is registration</div>;
+	const [username, setUsername] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [confirmPassword, setConfirmPassword] = useState("");
+	const [error, setError] = useState("");
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setError("");
+		// perform form validation and submission
+		if (password !== confirmPassword) {
+			setError("Passwords do not match");
+			return;
+		}
+		if (!/^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/.test(username)) {
+			setError("User Name Not Valid ");
+			return;
+		} else {
+			setError("");
+			// perform form submission
+		}
+	};
+	const userInfo = useContext(AuthContext);
+
+	return (
+		<div className="min-h-screen bg-gray-100 flex items-center justify-center w-4/6">
+			<div className="bg-white p-6 w-[40%] rounded shadow-md flex items-center flex-col">
+				<h2 className="text-2xl font-bold mb-4">Register</h2>
+
+				<form onSubmit={handleSubmit} className="w-8/12">
+					<div className="mb-4">
+						<label
+							className="block text-gray-700 font-bold mb-2"
+							htmlFor="username"
+						>
+							Username
+						</label>
+						<input
+							className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+							type="text"
+							placeholder="Enter your username"
+							value={username}
+							onChange={(e) => setUsername(e.target.value)}
+							required
+						/>
+					</div>
+					<div className="mb-4">
+						<label
+							className="block text-gray-700 font-bold mb-2"
+							htmlFor="email"
+						>
+							Email
+						</label>
+						<input
+							className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+							type="email"
+							placeholder="Enter your email"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+							required
+						/>
+					</div>
+					<div className="mb-4">
+						<label
+							className="block text-gray-700 font-bold mb-2"
+							htmlFor="password"
+						>
+							Password
+						</label>
+						<input
+							className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+							type="password"
+							placeholder="Enter your password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+							required
+						/>
+					</div>
+					<div className="mb-6">
+						<label
+							className="block text-gray-700 font-bold mb-2"
+							htmlFor="confirmPassword"
+						>
+							Confirm Password
+						</label>
+						<input
+							className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+							type="password"
+							placeholder="Confirm your password"
+							value={confirmPassword}
+							onChange={(e) => setConfirmPassword(e.target.value)}
+							required
+						/>
+					</div>
+					<div className="flex justify-center">
+						<button
+							className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+							type="submit"
+						>
+							Register
+						</button>
+					</div>
+					{error && (
+						<div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mb-4">
+							{error}
+						</div>
+					)}
+				</form>
+			</div>
+		</div>
+	);
 };
 
 export default Registration;
